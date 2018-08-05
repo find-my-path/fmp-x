e91b0214d2c7242e3736b658d594110ceb3a65e8
baldwinjj <baldwinjj@g.cofc.edu>
2018-08-05 15:34:29 -0400

    add some configurations

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(express.static(`${__dirname}/dist/fmp-x`))
-app.get('/', (req, res) => {
+// Configure Endpoints
+app.get('/api/', (req, res) => {
     res.send('Hello, World!')
 })
+
+// Serve Static Content
+app.use(express.static(`${__dirname}/dist`))
+app.all("/*", function(req, res, next) {
+    res.sendfile(`${__dirname}/dist/index.html`);
+});
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
